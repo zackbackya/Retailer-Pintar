9c6766e545520c7437ea28d2b4b397dd167cd3df
Mochammad Adibatuszackya <zbackyaa@gmail.com>
2021-10-10 23:11:34 +0700

    checkpoint save

---
+++ RetailerPintar/Referensi/Supplier/SupplierFrm.vb
@@ -0,0 +1,5 @@
+﻿Public Class SupplierFrm
+    Private Sub lblHeader_Click(sender As Object, e As EventArgs) Handles lblHeader.Click
+
+    End Sub
+End Class
---
+++ RetailerPintar/Referensi/Supplier/TambahSupplierFrm.vb
@@ -0,0 +1,3 @@
+﻿Public Class TambahSupplierFrm
+
+End Class
